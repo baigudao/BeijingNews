53961945a07d34109c75090725774364ada0d372
jackie <971060378@qq.com>
2017-01-03 13:21:22 +0800

    侧滑的实现

---
+++ app/src/main/java/com/jackie/beijingnews/fragment/ContentFragment.java
@@ -0,0 +1,15 @@
+package com.jackie.beijingnews.fragment;
+
+import android.view.View;
+
+import com.jackie.beijingnews.base.BaseFragment;
+
+/**
+ * 作用：正文Fragment
+ */
+public class ContentFragment extends BaseFragment {
+    @Override
+    public View initView() {
+        return null;
+    }
+}
